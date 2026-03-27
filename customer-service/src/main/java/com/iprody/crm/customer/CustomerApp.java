c773c50c6e48c62025e672bef9798160e007caed
mironenko <mironenko@protei.ru>
2026-03-27 19:23:00 +0300

    init - СheckStyle added

--- customer-service/src/main/java/com/iprody/crm/customer/CustomerApp.java
+++ customer-service/src/main/java/com/iprody/crm/customer/CustomerApp.java
@@ -3,9 +3,18 @@ package com.iprody.crm.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Start customer app class.
+ */
 @SpringBootApplication
 public class CustomerApp {
-    public static void main(String[] args) {
+
+    /**
+     * Start.
+     *
+     * @param args - arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(CustomerApp.class, args);
         System.out.println("Customer service started!");
     }
--- payment-service/src/main/java/com/iprody/crm/payment/PaymentApp.java
+++ payment-service/src/main/java/com/iprody/crm/payment/PaymentApp.java
@@ -3,9 +3,18 @@ package com.iprody.crm.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Start customer app class.
+ */
 @SpringBootApplication
 public class PaymentApp {
-    public static void main(String[] args) {
+
+    /**
+     * Start.
+     *
+     * @param args - arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(PaymentApp.class, args);
         System.out.println("Payment service started!");
     }
