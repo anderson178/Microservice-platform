ed8d8df08e0a346c45a7224a66be3d1e28bd0092
mironenko <mironenko@protei.ru>
2026-05-19 00:22:46 +0300

    issue-20: Monitoring support

---
+++ customer-service/src/main/java/com/iprody/customer/config/MetricsConfig.java
@@ -0,0 +1,19 @@
+package com.iprody.customer.config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.micrometer.metrics.autoconfigure.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Value("${spring.application.name:customer-service}")
+    private String applicationName;
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", applicationName);
+    }
+}
